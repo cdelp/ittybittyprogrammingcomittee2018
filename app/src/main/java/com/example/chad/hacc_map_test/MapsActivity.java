e5a84d9b35cbf7c7a330163dd0929c9c4219e697
cdelp <cdelp@hawaii.edu>
2018-11-03 23:26:57 -1000

    changed to fused location provider

--- app/src/main/java/com/example/chad/hacc_map_test/MapsActivity.java
+++ app/src/main/java/com/example/chad/hacc_map_test/MapsActivity.java
@@ -1,26 +1,31 @@
 package com.example.chad.hacc_map_test;
 
+// references:
+// https://javapapers.com/android/android-location-fused-provider/
+
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
-import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.location.Location;
-import android.location.LocationManager;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-import android.support.design.widget.BottomNavigationView;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -28,39 +33,47 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.util.List;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleMap mMap;
-    private LocationManager locationManager;
-    private Location location;
-    private TextView mTextMessage;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private static final String TAG = "LocationActivity";
+    private static final long INTERVAL = 1000 * 10;
+    private static final long FASTEST_INTERVAL = 1000 * 5;
+    LocationRequest mLocationRequest;
+    GoogleApiClient mGoogleApiClient;
+    Location mCurrentLocation;
+    String mLastUpdateTime;
     private SensorManager sensorManager;
     private int toastDuration = Toast.LENGTH_SHORT;
+    private ArrayList locationsList;
+    private int northernMostIndex;
+    private int southernMostIndex;
+    private int westernMostIndex;
+    private int easternMostIndex;
 
     private boolean logGPS = false;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+    /**
+     *
+     */
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
 
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-            switch (item.getItemId()) {
-                case R.id.Stop:
-                    //mTextMessage.setText(R.string.title_home);
-                    return true;
-
-                case R.id.Start:
-
-                    mTextMessage.setText("Tracking");
-                    return true;
-            }
-            return false;
-        }
-    };
-
+    /**
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -70,7 +83,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // list of all locations
+        locationsList = new ArrayList();
+
+        createLocationRequest();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
@@ -84,23 +117,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             // for ActivityCompat#requestPermissions for more details.
             Log.e("HACC", "Location permission not granted");
             return;
+        } else {
+            Log.e("HACC", "Location permission granted");
         }
-        // move where needed
-        location = locationManager.getLastKnownLocation(locationManager.NETWORK_PROVIDER);
-        //onLocationChanged(location);
+
 
         //TODO testing only, use sensors if needed
         // Get list of sensors
-        sensorManager= (SensorManager) getSystemService(SENSOR_SERVICE);
-        List<Sensor> msensorList = sensorManager.getSensorList(Sensor.TYPE_ALL);
-        Log.d("HACC", msensorList.toString());
+//        sensorManager= (SensorManager) getSystemService(SENSOR_SERVICE);
+//        List<Sensor> msensorList = sensorManager.getSensorList(Sensor.TYPE_ALL);
+//        Log.d("HACC", msensorList.toString());
 
         Button startButton = findViewById(R.id.Start);
         startButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // trigger logging of gps and sensors
                 logGPS = true;
-                onLocationChanged(location);
+                //onLocationChanged(location);
 
                 Log.d("HACC", "start button pressed");
                 CharSequence startText = "Starting Tracking";
@@ -123,8 +156,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 toast.show();
             }
         });
-
-        mTextMessage = (TextView) findViewById(R.id.message);
     }
 
 
@@ -147,19 +178,104 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 //        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
+    /**
+     * Triggered when GPS or Network location changes
+     * @param location
+     */
     @Override
     public void onLocationChanged(Location location) {
+        Log.d("HACC", "onLocationChanged");
+
+        mCurrentLocation = location;
+        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+        Log.d("HACC", "last update: " + mLastUpdateTime);
+
         double lat = location.getLatitude();
         double lon = location.getLongitude();
         LatLng latLng = new LatLng( lat, lon );
         Log.d("HACC", "lat: " + lat + ", lon: " + lon);
+        Log.d("HACC", "location list: " + locationsList.size());
 
         if (logGPS) {
             Log.d("HACC", "GPS being logged");
+            //TODO, consider replacing arraylist with db
+            locationsList.add(location);
             mMap.addMarker(new MarkerOptions().position(latLng).title("current position"));
             mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+            mMap.moveCamera(CameraUpdateFactory.zoomTo(17));
         } else {
             Log.d("HACC", "GPS NOT being logged");
         }
     }
+
+    protected void startLocationUpdates() {
+        PendingResult<Status> pendingResult = LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+        Log.d("HACC", "Location update started");
+    }
+
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleApiClient, this);
+        Log.d(TAG, "Location update stopped");
+    }
+
+    /**
+     *
+     * @param bundle
+     */
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d("HACC", "onConnected - isConnected: " + mGoogleApiClient.isConnected());
+        startLocationUpdates();
+    }
+
+    /**
+     *
+     * @param i
+     */
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    /**
+     *
+     * @param connectionResult
+     */
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d("HACC", "onStart");
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d("HACC", "onStop");
+        mGoogleApiClient.disconnect();
+        Log.d("HACC", "isConnected: " + mGoogleApiClient.isConnected());
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mGoogleApiClient.isConnected()) {
+            startLocationUpdates();
+            Log.d(TAG, "Location update resumed");
+        }
+    }
+
 }
